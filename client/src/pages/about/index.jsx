b675372f5f5b451ebf411b23028cedf7addafec0
Clyde Baron Rapinan <clydebaron2000@gmail.com>
2021-09-10 17:28:51 -0700

    about section formatted

--- client/src/pages/about/index.jsx
+++ client/src/pages/about/index.jsx
@@ -1,67 +1,107 @@
+import Footer from "../../components/footer"
 import  profile from "../../media/Glen Lubao 08252021 57.jpg"
 export default function About(props){
     // console.log(profile)
+    if (true)
     return <>
-        <div>
-            <img className='profile-pic' src={profile} alt='aaaaa profile'/>
-            <div>
-                Providing in-person, telehealth, or virtual remote certified medical-legal
-                language interpretation services (Tagalog-English) for some cities in California like San Diego County, Riverside County, Orange County, Los Angeles County
-                and San Francisco (Virtual Remote, VRI).
-                The type of appointments I have had provided services for were mostly
-                worker’s compensation related cases like medical-legal exams, QME, IME, AME
-                & PQME including administrative hearings & depositions but not limited to the
-                following.
-                Therapy Appointments like physical therapy, acupuncture, chiropractic, psyche, behavioral or cognitive behavioral therapy, aqua, and speech therapy including
-                pain management program related appointments.
-                Medical treatment appointments and follow-up visits with pre & post-surgery
-                medical examination, imaging, MRI CT X-Ray, nerve tests, EEG, EKG.
-                Translation of documents like medical consent forms, advance directives,
-                compromise & release (C&R), and court-recorded transcripts.
-            </div>
+    <div className='about'>
+        <section>
+                <div className='name'>
+                    <h1>
+                        Glen Lubao
+                    </h1>
+                    <h2>
+                        Certified Interpreter
+                    </h2>
+                </div>
+                <div className='description'>
+                    <div className='box'>
+                        <div className='img-container'>
+                            <img   
+                                className='profile-pic' src={profile} alt='aaaaa profile'/>
+                        </div>
+                    </div>
+                    <div className='box'>
+                        <p>
+                            Providing in-person, telehealth, or virtual remote certified medical-legal
+                            language interpretation services (Tagalog-English) for some cities in California like San Diego County, Riverside County, Orange County, Los Angeles County
+                            and San Francisco (Virtual Remote, VRI).
+                            The type of appointments I have had provided services for were mostly
+                            worker’s compensation related cases like medical-legal exams, QME, IME, AME
+                            & PQME including administrative hearings & depositions but not limited to the
+                            following.
+                        </p>
+                        <p>
+                            Therapy Appointments like physical therapy, acupuncture, chiropractic, psyche, behavioral or cognitive behavioral therapy, aqua, and speech therapy including
+                            pain management program related appointments.
+                            Medical treatment appointments and follow-up visits with pre & post-surgery
+                            medical examination, imaging, MRI CT X-Ray, nerve tests, EEG, EKG.
+                            Translation of documents like medical consent forms, advance directives,
+                            compromise & release (C&R), and court-recorded transcripts.
+                        </p>
+                    </div>
+                </div>
+        </section>
+        <section>
+            <h3>Education</h3>
             <ul>
                 <li>
-                    Education
-                    Bachelor’s Degree of Science in Electronics and Communication Engineering
-                    University of the East, Philippines -1982-1987
+                    <h4>Bachelor’s Degree of Science in Electronics and Communication Engineering</h4>
+                    <p>University of the East, Philippines -1982-1987</p>
+                </li>    
+                <li>
+                    <h4>Completion of training on Fundamentals of Interpreting, Standards of Practice Code of Ethics, Cultural Awareness, Medical Terminology & Skill Building in Three Modes of Interpretation. </h4>
+                    <p>Medical Interpreter Training School (MITS)</p>
+                </li>      
+                <li>
+                    <h4>
+                        Completion of Court Interpreter Training, Legal Terminology, Ethical Responsibility Code of Ethics & Professional responsibility.
+                    </h4>
+                    <p>
+                        De La Mora Interpreter Training (DMIT) 
+                    </p>
                 </li>
                 <li>
-                    Medical Interpreter Training School (MITS)  
-                    Completion of training on Fundamentals of Interpreting, Standards of Practice Code of Ethics, Cultural Awareness, Medical Terminology & Skill Building in Three Modes of Interpretation. 
-                </li>
-                <li>
-                    De La Mora Interpreter Training (DMIT) 
-                    Completion of Court Interpreter Training, Legal Terminology, Ethical Responsibility Code of Ethics & Professional responsibility.
-                </li>
-                <li>
-                    Medication Management Course Completion
-                    Ron’s Pharmacy Services
-                </li>
-                <li>
-                    Certification
-                    CoreCHI Cert. No. xxx825 (Exp. 06/28/2022)
-                    Certification Commission for Healthcare Interpreters (CCHI), Washington, D.C.
+                    <h4>
+                        Medication Management Course Completion
+                    </h4>
+                    <p>
+                        Ron’s Pharmacy Services
+                    </p>
                 </li>
             </ul>
+        </section>
+        <section>
             <div>
-                Professional Affiliations
+                <h3>Certification</h3>
+                <ul>
+                    <li>
+                        <h4>CoreCHI Cert. No. xxx825 (Exp. 06/28/2022)</h4>
+                        <p>Certification Commission for Healthcare Interpreters (CCHI), Washington, D.C.</p>
+                    </li>
+                </ul>
             </div>
-            <ul>
-                <li>
-                    San Diego Certified Medical Interpreters Network, SDCMIN
-                    Former Board Member
-                </li>
-                <li>
-                    California Federation of Interpreters, CFI
-                    Member
-                </li>
-                <li>
-                    National Council on Interpreting in Health Care, NCIHC
-                    Washington, DC – Member
-                </li>
-            </ul>
-        </div>
-    
-    
+        </section>
+        <section>
+            <div>
+                <h3>Professional Affiliations</h3>
+                <ul>
+                    <li>
+                        <h4>San Diego Certified Medical Interpreters Network, SDCMIN</h4>
+                        <p>Former Board Member</p>
+                    </li>
+                    <li>
+                        <h4>California Federation of Interpreters, CFI</h4>
+                        <p>Member</p>
+                    </li>
+                    <li>
+                        <h4>National Council on Interpreting in Health Care, NCIHC</h4>
+                        <p>Washington, DC – Member</p>
+                    </li>
+                </ul>
+            </div>
+        </section>
+    </div>
+    <Footer/>
     </>
 }
